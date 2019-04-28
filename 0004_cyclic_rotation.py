c36b848512522b253d3431350441744d66e8859f
Mani AHMADI ARSHAM <moneyfx@gmail.com>
2019-04-28 14:19:34 -0400

    rotate array

---
+++ 0004_cyclic_rotation.py
@@ -0,0 +1,14 @@
+# https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+# https://app.codility.com/demo/results/training929UJV-7J3/
+def solution(A, K):
+    l = len(A)
+    arr = [0] * l
+    
+    for i in range(0, l):
+        if i + K < l:
+            arr[i+K] = A[i]
+        else:
+            idx = (i + K) % l
+            arr[idx] = A[i] 
+    return arr
+ 
